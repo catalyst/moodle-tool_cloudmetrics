ef5077ccf27f8b33614299a7044a816b795d0375
dustinhuynh <dustinhuynh@catalyst-au.net>
2025-08-18 14:03:35 +1000

    Fix display bug with metrics #197

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025072300;
-$plugin->release = 2025072300;
+$plugin->version = 2025072301;
+$plugin->release = 2025072301;
 
 $plugin->requires = 2024042200;    // Our lowest supported Moodle (4.4.0).
 
