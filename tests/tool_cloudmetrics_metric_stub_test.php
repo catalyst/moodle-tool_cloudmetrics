0efe8d83c53fa740121bcb5cebb147b6eb63f315
jasondendulk <jasondendulk@catalyst-au.net>
2026-02-03 14:51:23 +1100

    Issue #207 - Extend test metrics to include backfilling.

--- tests/tool_cloudmetrics_metric_stub_test.php
+++ tests/tool_cloudmetrics_metric_stub_test.php
@@ -16,20 +16,27 @@
 
 namespace tool_cloudmetrics;
 
+use PHPUnit\Framework\Attributes\CoversClass;
+use tool_cloudmetrics\metric\manager;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once(__DIR__ . "/metric_testcase.php"); // This is needed. File will not be automatically included.
 
 /**
- * Basic test for collectors.
+ * Tests that the metric mock classes work.
  *
  * @package   tool_cloudmetrics
  * @author    Jason den Dulk <jasondendulk@catalyst-au.net>
  * @copyright 2022, Catalyst IT
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+#[CoversClass(metric_testcase::class)]
 final class tool_cloudmetrics_metric_stub_test extends metric_testcase {
-    public function test_get_stub() {
+    /**
+     * Tests the basic stub.
+     */
+    public function test_get_stub(): void {
         $stub = $this->get_metric_stub([1, 2, 3]);
 
         $time = 100;
@@ -51,5 +58,33 @@ final class tool_cloudmetrics_metric_stub_test extends metric_testcase {
         $i = $stub->generate_metric_item(0, $time);
         $this->assertEquals(1, $i->value);
         $this->assertEquals($time, $i->time);
+    }
+
+    /**
+     * Tests that the backfillable test stub works.
+     */
+    public function test_get_backfillable_metric_stub(): void {
+        $cycle = [1, 2, 3];
+        $infinite = new \InfiniteIterator(new \ArrayIterator($cycle));
+        $infinite->rewind();
+
+        $frequency = manager::FREQ_MIN;
+        $stub = $this->get_backfillable_metric_stub($cycle, $frequency);
+        $backtime = 240;
+        $finish = time();
+        $start = $finish - $backtime;
+
+        $items = $stub->generate_metric_items($backtime, $finish, $frequency);
+
+        $time = $start;
+        foreach ($items as $item) {
+            $value = $infinite->current();
+            $infinite->next();
+
+            $this->assertEquals($value, $item->value);
+            $this->assertEquals($time, $item->time);
+            $time = lib::get_next_time($time, $frequency);
+        }
+        $this->assertGreaterThan($finish, $time);
     }
 }
